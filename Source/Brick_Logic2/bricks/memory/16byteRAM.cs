99840566f646179dbd704b2e83e93d1e2df00ae1
Auios <LoneAuios@gmail.com>
2018-01-19 16:30:37 -0500

    Changed pin labels for RAM

--- Source/Brick_Logic2/bricks/memory/16byteRAM.cs
+++ Source/Brick_Logic2/bricks/memory/16byteRAM.cs
@@ -17,69 +17,69 @@ datablock fxDTSBrickData (Ram16byteBrickData : Powerbrick1x1Data)//base
 
 	//Main Outputs
 	numPE = 8;
-	PEName[0] = "Out0";
+	PEName[0] = "Out7";
 	PEPos[0] = "-3.75 0.75 0.0";
 	PEScale[0] = "0.5 0.5 0.16666";
 
-	PEName[1] = "Out1";
+	PEName[1] = "Out6";
 	PEPos[1] = "-3.25 0.75 0.0";
 	PEScale[1] = "0.5 0.5 0.16666";
 
-	PEName[2] = "Out2";
+	PEName[2] = "Out5";
 	PEPos[2] = "-2.75 0.75 0.0";
 	PEScale[2] = "0.5 0.5 0.16666";
 
-	PEName[3] = "Out3";
+	PEName[3] = "Out4";
 	PEPos[3] = "-2.25 0.75 0.0";
 	PEScale[3] = "0.5 0.5 0.16666";
 
-	PEName[4] = "Out4";
+	PEName[4] = "Out3";
 	PEPos[4] = "-1.75 0.75 0.0";
 	PEScale[4] = "0.5 0.5 0.16666";
 
-	PEName[5] = "Out5";
+	PEName[5] = "Out2";
 	PEPos[5] = "-1.25 0.75 0.0";
 	PEScale[5] = "0.5 0.5 0.16666";
 
-	PEName[6] = "Out6";
+	PEName[6] = "Out1";
 	PEPos[6] = "-0.75 0.75 0.0";
 	PEScale[6] = "0.5 0.5 0.16666";
 
-	PEName[7] = "Out7";
+	PEName[7] = "Out0";
 	PEPos[7] = "-0.25 0.75 0.0";
 	PEScale[7] = "0.5 0.5 0.16666";
 
 	//Main Inputs
 	numIE = 15;
-	IEName[0] = "In0";
+	IEName[0] = "In7";
 	IEPos[0] = "-3.75 -0.75 0.0";
 	IEScale[0] = "0.5 0.5 0.16666";
 
-	IEName[1] = "In1";
+	IEName[1] = "In6";
 	IEPos[1] = "-3.25 -0.75 0.0";
 	IEScale[1] = "0.5 0.5 0.16666";
 
-	IEName[2] = "In2";
+	IEName[2] = "In5";
 	IEPos[2] = "-2.75 -0.75 0.0";
 	IEScale[2] = "0.5 0.5 0.16666";
 
-	IEName[3] = "In3";
+	IEName[3] = "In4";
 	IEPos[3] = "-2.25 -0.75 0.0";
 	IEScale[3] = "0.5 0.5 0.16666";
 
-	IEName[4] = "In4";
+	IEName[4] = "In3";
 	IEPos[4] = "-1.75 -0.75 0.0";
 	IEScale[4] = "0.5 0.5 0.16666";
 
-	IEName[5] = "In5";
+	IEName[5] = "In2";
 	IEPos[5] = "-1.25 -0.75 0.0";
 	IEScale[5] = "0.5 0.5 0.16666";
 
-	IEName[6] = "In6";
+	IEName[6] = "In1";
 	IEPos[6] = "-0.75 -0.75 0.0";
 	IEScale[6] = "0.5 0.5 0.16666";
 
-	IEName[7] = "In7";
+	IEName[7] = "In0";
 	IEPos[7] = "-0.25 -0.75 0.0";
 	IEScale[7] = "0.5 0.5 0.16666";
 
@@ -99,19 +99,19 @@ datablock fxDTSBrickData (Ram16byteBrickData : Powerbrick1x1Data)//base
 	IEScale[10] = "0.5 0.5 0.16666";
 
 	//Address Pins
-	IEName[11] = "Address0";
+	IEName[11] = "Address3";
 	IEPos[11] = "1.25 -0.75 0.0";
 	IEScale[11] = "0.5 0.5 0.16666";
 
-	IEName[12] = "Address1";
+	IEName[12] = "Address2";
 	IEPos[12] = "1.75 -0.75 0.0";
 	IEScale[12] = "0.5 0.5 0.16666";
 
-	IEName[13] = "Address2";
+	IEName[13] = "Address1";
 	IEPos[13] = "2.25 -0.75 0.0";
 	IEScale[13] = "0.5 0.5 0.16666";
 
-	IEName[14] = "Address3";
+	IEName[14] = "Address0";
 	IEPos[14] = "2.75 -0.75 0.0";
 	IEScale[14] = "0.5 0.5 0.16666";
 };
@@ -134,7 +134,7 @@ function Ram16byteBrickData::DoLog(%data,%gate,%statestack,%client)
 			%gate.data[%gate.currAddr,6] = %statestack.ins[6];
 			%gate.data[%gate.currAddr,7] = %statestack.ins[7];
 		}
-		
+
 		if(%statestack.ins[9])//read enabled
 		{
 			SetPEPowered(%gate.PE[0],%gate.data[%gate.currAddr,0],%client);
@@ -148,3 +148,49 @@ function Ram16byteBrickData::DoLog(%data,%gate,%statestack,%client)
 		}
 	}
 }
+
+
+//function Ram16byteBrickData::DoLog(%data,%gate,%statestack,%client)
+//{
+//	//first figure out the address
+//	%gate.currAddr = (%statestack.ins[11] * 1) + (%statestack.ins[12] * 2) + (%statestack.ins[13] * 4) + (%statestack.ins[14] * 8);
+//
+//	if(!%gate.IE[10].previousState && %statestack.ins[10])//positive clock edge.
+//	{
+//		if(%statestack.ins[8])//if write enable
+//		{
+//			%arrIdx = 0;
+//			%gate.data[%gate.currAddr,%arrIdx] = %statestack.ins[%arrIdx++];
+//			%gate.data[%gate.currAddr,%arrIdx] = %statestack.ins[%arrIdx++];
+//			%gate.data[%gate.currAddr,%arrIdx] = %statestack.ins[%arrIdx++];
+//			%gate.data[%gate.currAddr,%arrIdx] = %statestack.ins[%arrIdx++];
+//			%gate.data[%gate.currAddr,%arrIdx] = %statestack.ins[%arrIdx++];
+//			%gate.data[%gate.currAddr,%arrIdx] = %statestack.ins[%arrIdx++];
+//			%gate.data[%gate.currAddr,%arrIdx] = %statestack.ins[%arrIdx++];
+//			%gate.data[%gate.currAddr,%arrIdx] = %statestack.ins[%arrIdx++];
+//		}
+//		
+//		if(%statestack.ins[9])//read enabled
+//		{
+//			%arrIdx = 0;
+//			%gate.data[%arrIdx] = %gate.data[%gate.currAddr, %arrIdx++];
+//			%gate.data[%arrIdx] = %gate.data[%gate.currAddr, %arrIdx++];
+//			%gate.data[%arrIdx] = %gate.data[%gate.currAddr, %arrIdx++];
+//			%gate.data[%arrIdx] = %gate.data[%gate.currAddr, %arrIdx++];
+//			%gate.data[%arrIdx] = %gate.data[%gate.currAddr, %arrIdx++];
+//			%gate.data[%arrIdx] = %gate.data[%gate.currAddr, %arrIdx++];
+//			%gate.data[%arrIdx] = %gate.data[%gate.currAddr, %arrIdx++];
+//			%gate.data[%arrIdx] = %gate.data[%gate.currAddr, %arrIdx++];
+//		}
+//	}
+//
+//	%arrIdx = 0;
+//	SetPEPowered(%gate.PE[%arrIdx], %gate.data[%arrIdx++], %client);
+//	SetPEPowered(%gate.PE[%arrIdx], %gate.data[%arrIdx++], %client);
+//	SetPEPowered(%gate.PE[%arrIdx], %gate.data[%arrIdx++], %client);
+//	SetPEPowered(%gate.PE[%arrIdx], %gate.data[%arrIdx++], %client);
+//	SetPEPowered(%gate.PE[%arrIdx], %gate.data[%arrIdx++], %client);
+//	SetPEPowered(%gate.PE[%arrIdx], %gate.data[%arrIdx++], %client);
+//	SetPEPowered(%gate.PE[%arrIdx], %gate.data[%arrIdx++], %client);
+//	SetPEPowered(%gate.PE[%arrIdx], %gate.data[%arrIdx++], %client);
+//}*/
