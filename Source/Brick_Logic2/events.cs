585de95e8ccd0e23efc5f74ec9432940c46ae183
Eagle517 <15026800+Eagle517@users.noreply.github.com>
2018-01-20 00:45:36 -0600

    onPower events no longer admin only

--- Source/Brick_Logic2/events.cs
+++ Source/Brick_Logic2/events.cs
@@ -1,8 +1,7 @@
 //events.cs
-
-registerInputEvent("fxDTSBrick","onPowerChange","Self fxDTSBrick" TAB "Client GameConnection" TAB "Player Player",1);
-registerInputEvent("fxDTSBrick","onPowerOn","Self fxDTSBrick" TAB "Client GameConnection" TAB "Player Player",1);
-registerInputEvent("fxDTSBrick","onPowerOff","Self fxDTSBrick" TAB "Client GameConnection" TAB "Player Player",1);
+registerInputEvent("fxDTSBrick", "onPowerChange", "Self fxDTSBrick" TAB "Client GameConnection" TAB "Player Player", 0);
+registerInputEvent("fxDTSBrick", "onPowerOn", "Self fxDTSBrick" TAB "Client GameConnection" TAB "Player Player", 0);
+registerInputEvent("fxDTSBrick", "onPowerOff", "Self fxDTSBrick" TAB "Client GameConnection" TAB "Player Player", 0);
 
 registerOutputEvent(fxDTSBrick, SetPower, "string 30 50", 1);
 registerOutputEvent(fxDTSBrick, SetIsWire, "bool 1", 1);
