1809818c2ec8fda81aacc1ea7a6894e7ea3c58c6
Auios <LoneAuios@gmail.com>
2018-02-18 22:12:36 -0500

    Expanded multiplication output

--- Source/Brick_Logic2/bricks/arithmetic/8bitMultiplier.cs
+++ Source/Brick_Logic2/bricks/arithmetic/8bitMultiplier.cs
@@ -12,47 +12,87 @@ datablock fxDTSBrickData (Multiplier8bitBrick4x16Data : Powerbrick1x1Data)
 	TipInfo = "Multiplies 8bit A and 8bit B together and produces a product";
 	ISINSTANT = 0;
 
-	numPE = 8;
+	numPE = 16;
 
 	PEName[0] = "Out0";
-	PEPos[0] = "-0.25 0.75 0.0";
+	PEPos[0] = "3.75 0.75 0.0";
 	PEScale[0] = "0.5 0.5 0.16666";
 	PEStart[0] = 0;	
 
 	PEName[1] = "Out1";
-	PEPos[1] = "-0.75 0.75 0.0";
+	PEPos[1] = "3.25 0.75 0.0";
 	PEScale[1] = "0.5 0.5 0.16666";
-	PEStart[1] = 0;	
+	PEStart[1] = 0;
 
 	PEName[2] = "Out2";
-	PEPos[2] = "-1.25 0.75 0.0";
+	PEPos[2] = "2.75 0.75 0.0";
 	PEScale[2] = "0.5 0.5 0.16666";
-	PEStart[2] = 0;	
+	PEStart[2] = 0;
 
 	PEName[3] = "Out3";
-	PEPos[3] = "-1.75 0.75 0.0";
+	PEPos[3] = "2.25 0.75 0.0";
 	PEScale[3] = "0.5 0.5 0.16666";
-	PEStart[3] = 0;	
+	PEStart[3] = 0;
 
 	PEName[4] = "Out4";
-	PEPos[4] = "-2.25 0.75 0.0";
+	PEPos[4] = "1.75 0.75 0.0";
 	PEScale[4] = "0.5 0.5 0.16666";
-	PEStart[4] = 0;	
+	PEStart[4] = 0;
 
 	PEName[5] = "Out5";
-	PEPos[5] = "-2.75 0.75 0.0";
+	PEPos[5] = "1.25 0.75 0.0";
 	PEScale[5] = "0.5 0.5 0.16666";
-	PEStart[5] = 0;	
+	PEStart[5] = 0;
 
 	PEName[6] = "Out6";
-	PEPos[6] = "-3.25 0.75 0.0";
+	PEPos[6] = "0.75 0.75 0.0";
 	PEScale[6] = "0.5 0.5 0.16666";
-	PEStart[6] = 0;	
+	PEStart[6] = 0;
 
 	PEName[7] = "Out7";
-	PEPos[7] = "-3.75 0.75 0.0";
+	PEPos[7] = "0.25 0.75 0.0";
 	PEScale[7] = "0.5 0.5 0.16666";
 	PEStart[7] = 0;
+
+	PEName[8] = "Out8";
+	PEPos[8] = "-0.25 0.75 0.0";
+	PEScale[8] = "0.5 0.5 0.16666";
+	PEStart[8] = 0;
+
+	PEName[9] = "Out9";
+	PEPos[9] = "-0.75 0.75 0.0";
+	PEScale[9] = "0.5 0.5 0.16666";
+	PEStart[9] = 0;
+
+	PEName[10] = "Out10";
+	PEPos[10] = "-1.25 0.75 0.0";
+	PEScale[10] = "0.5 0.5 0.16666";
+	PEStart[10] = 0;
+
+	PEName[11] = "Out11";
+	PEPos[11] = "-1.75 0.75 0.0";
+	PEScale[11] = "0.5 0.5 0.16666";
+	PEStart[11] = 0;
+
+	PEName[12] = "Out12";
+	PEPos[12] = "-2.25 0.75 0.0";
+	PEScale[12] = "0.5 0.5 0.16666";
+	PEStart[12] = 0;
+
+	PEName[13] = "Out13";
+	PEPos[13] = "-2.75 0.75 0.0";
+	PEScale[13] = "0.5 0.5 0.16666";
+	PEStart[13] = 0;
+
+	PEName[14] = "Out14";
+	PEPos[14] = "-3.25 0.75 0.0";
+	PEScale[14] = "0.5 0.5 0.16666";
+	PEStart[14] = 0;
+
+	PEName[15] = "Out15";
+	PEPos[15] = "-3.75 0.75 0.0";
+	PEScale[15] = "0.5 0.5 0.16666";
+	PEStart[15] = 0;
 
 	numIE = 16;
 
@@ -182,12 +222,20 @@ function Multiplier8bitBrick4x16Data::DoLog(%data, %gate, %statestack, %client)
     //    SetPEPowered(%gate.PE[%i], (%finalValue & (1 << %i) > 0), %client);
     //}
 
-    SetPEPowered(%gate.PE[0], %finalValue & 1, %client);
-    SetPEPowered(%gate.PE[1], %finalValue & 2, %client);
-    SetPEPowered(%gate.PE[2], %finalValue & 4, %client);
-    SetPEPowered(%gate.PE[3], %finalValue & 8, %client);
-    SetPEPowered(%gate.PE[4], %finalValue & 16, %client);
-    SetPEPowered(%gate.PE[5], %finalValue & 32, %client);
-    SetPEPowered(%gate.PE[6], %finalValue & 64, %client);
-    SetPEPowered(%gate.PE[7], %finalValue & 128, %client);
+    SetPEPowered(%gate.PE[0],  %finalValue & 1, %client);
+    SetPEPowered(%gate.PE[1],  %finalValue & 2, %client);
+    SetPEPowered(%gate.PE[2],  %finalValue & 4, %client);
+    SetPEPowered(%gate.PE[3],  %finalValue & 8, %client);
+    SetPEPowered(%gate.PE[4],  %finalValue & 16, %client);
+    SetPEPowered(%gate.PE[5],  %finalValue & 32, %client);
+    SetPEPowered(%gate.PE[6],  %finalValue & 64, %client);
+    SetPEPowered(%gate.PE[7],  %finalValue & 128, %client);
+    SetPEPowered(%gate.PE[8],  %finalValue & 256, %client);
+    SetPEPowered(%gate.PE[9],  %finalValue & 512, %client);
+    SetPEPowered(%gate.PE[10], %finalValue & 1024, %client);
+    SetPEPowered(%gate.PE[11], %finalValue & 2048, %client);
+    SetPEPowered(%gate.PE[12], %finalValue & 4096, %client);
+    SetPEPowered(%gate.PE[13], %finalValue & 8192, %client);
+    SetPEPowered(%gate.PE[14], %finalValue & 16384, %client);
+    SetPEPowered(%gate.PE[15], %finalValue & 32768, %client);
 }
